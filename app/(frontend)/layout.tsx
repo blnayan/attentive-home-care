4b7251f7ec7e2d55013f39b7640c6d5192889b9c
blnayan <52083057+blnayan@users.noreply.github.com>
2025-10-11 19:40:18 -0400

    add: payloadcms

---
+++ app/(frontend)/layout.tsx
@@ -0,0 +1,33 @@
+import type React from "react";
+import "@/app/(frontend)/globals.css";
+import { Inter } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Attentive Home Care, Inc. | Compassionate Home Care Services",
+  description:
+    "Providing high-quality non-medical home care services that improve the quality of life for every client in Northern Virginia.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
