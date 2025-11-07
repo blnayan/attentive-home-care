43f0e2a1bdc2167ebba30b4a82a502df441ba967
Nayan <52083057+blnayan@users.noreply.github.com>
2025-11-06 19:54:55 -0500

    Adjust InlineWidget height in BookAppointment component
    Set InlineWidget height to 100vh for better display.

--- app/(frontend)/book-appointment/page.tsx
+++ app/(frontend)/book-appointment/page.tsx
@@ -5,7 +5,12 @@ import { InlineWidget } from "react-calendly";
 export default function BookAppointment() {
   return (
     <div className="h-screen">
-      <InlineWidget url="https://calendly.com/bijonsingh381/new-meeting" />
+      <InlineWidget 
+        url="https://calendly.com/bijonsingh381/new-meeting"
+        styles={{
+          height: "100vh"
+        }}
+      />
     </div>
   );
 }
